2e9a756f9340d6fe232807d1a4850c4f8ca13cc1
Tutao1995 <851542486@qq.com>
2019-08-06 21:14:25 +0800

    swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="swiper">
+        <swiper :options="swiperOption" >
+            <swiper-slide v-for="slide of slideList" :key='slide.id'>
+                <img class="swiper-img" :src="slide.src"  :alt="slide.alt">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+        
+</template>
+<script>
+export default {
+    name:'HomeSwiper',
+    data(){
+        return{
+            swiperOption:{
+                pagination:{
+                    el:'.swiper-pagination'
+                },
+                loop:true
+            },
+            slideList:[
+                {   
+                    id:'001',
+                    src:'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/790fd1eacc298e09ae78025a03ab1c18.jpg_890x330_b2c44acb.jpg',
+                    alt:'上海杰尼斯'
+                },
+                {
+                    id:'002',
+                    src:'https://imgs.qunarzz.com/piao/fusion/1803/f2/928a8ff6148e5802.png_890x330_4c2f220f.png',
+                    alt:'北京石林'
+                },
+            ]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+.swiper >>> .swiper-pagination-bullet-active
+    background :#fff;
+.swiper
+    width:100%;
+    height 0;
+    overflow:hidden;
+    padding-bottom:31.25%;
+    background:#eee;
+    .swiper-img
+        width:100%;
+    .swiper-pagination
+        bottom:.6rem;
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <home-header></home-header>
-        home
+        <home-swiper></home-swiper>
+        <div>test</div>
     </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+
 export default {
     name:'home',
     components:{
-        HomeHeader
+        HomeHeader,
+        HomeSwiper
     }
 }
 </script>
